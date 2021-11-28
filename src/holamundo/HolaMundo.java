52fef7f30dab5fe98a8fe3764f31136771e21b8f
juabosque <juabosque11@gmail.com>
2021-11-27 18:16:18 -0700

    probandoPat

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,14 @@
+
+package holamundo;
+
+
+public class HolaMundo {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        System.out.println("HECHO POS JUAN BOSQUES");
+                
+    }
+    
+}
